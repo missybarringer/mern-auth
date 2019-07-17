bd733c6472cb121e4bd77ce416c2bd24c884f818
Margaret Barringer <margaretbarringer@MacBook-Pro.localdomain>
2019-07-17 18:26:06 -0500

    added initial files

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb+srv://missy123:missy123@mern-auth-zgrds.mongodb.net/test?retryWrites=true&w=majority"
+}
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+//bodyparser middleware
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+
+// DB config
+const db = require("./config/keys").mongoURI;
+
+//connect to mongoDB
+mongoose
+  .connect(
+      db,
+      { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+  const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+
+  app.listen(port, () => console.log(`Server up and running on port ${port} !`));
