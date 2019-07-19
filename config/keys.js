b8251f848574533b42a11e1e662ddb8606239699
Margaret Barringer <margaretbarringer@MacBook-Pro.localdomain>
2019-07-19 12:23:31 -0500

    added passport & user logic

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://missy123:missy123@mern-auth-zgrds.mongodb.net/test?retryWrites=true&w=majority"
-}
+    mongoURI: "mongodb+srv://missy123:missy123@mern-auth-zgrds.mongodb.net/test?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+};
