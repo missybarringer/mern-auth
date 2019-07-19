bf90ebffa7b143ba5af050132d876734de379195
Margaret Barringer <margaretbarringer@MacBook-Pro.localdomain>
2019-07-19 13:47:18 -0500

    added passport to server.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -99,3 +99,5 @@ router.post("/login", (req, res) => {
         });
     });
 });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const users = require("./routes/api/users");
 
 const app = express();
 
@@ -22,6 +25,15 @@ mongoose
   )
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
+
+  // passport middleware
+  app.use(passport.initialize());
+
+  //passport config
+  require("./config/passport")(passport);
+
+  //routes
+  app.use("/api/users", users);
 
   const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 
